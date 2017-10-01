f861e9815c54ebb1d497debca9a39ef961c98eaa
Dilyan Ivanov <dilyan.ivanov@gmail.com>
2017-10-01 11:20:47 +0300

    Updating with Data Types and Variables - Lab

--- Data Types and Variables/Problem 4.Elevator/Program.cs
+++ Data Types and Variables/Problem 4.Elevator/Program.cs
@@ -10,6 +10,14 @@ namespace Problem_4.Elevator
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int courses = (int)Math.Ceiling((double)n / p);
+            Console.WriteLine(courses);
+
         }
     }
 }
+//Calculate how many courses will be needed to elevate n persons by using an 
+//elevator of capacity of p persons.The input holds two lines: the number 
+//of people n and the capacity p of the elevator.
---
+++ Data Types and Variables/Problem 8. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_8.Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length, width, height = 0;
+            Console.Write("Length: ");
+            length = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            height = double.Parse(Console.ReadLine());
+            double volume = 0;
+            volume = (length * width * height) / 3;
+            Console.WriteLine("Pyramid Volume: {0:F2}", volume);
+
+        }
+    }
+}
