699eefa6e0a3c0a9636d958cd5a3861b2dc6b7ad
Dilyan Ivanov <dilyan.ivanov@gmail.com>
2017-10-05 18:11:49 +0300

    Adding some more tasks to Data Types and Variables - Exercises

---
+++ Data Types and Variables - Exercises/Some Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Some_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static void PrintHeader()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
